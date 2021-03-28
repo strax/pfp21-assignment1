b406b60e4da9582d397eff2b0b707f78514f91c4
Sami Kukkonen <sami@samikukkonen.fi>
2021-03-28 13:08:35 +0300

    Use memory_mapped_file in vbytedec

--- src/vbytedec.cpp
+++ src/vbytedec.cpp
@@ -18,14 +18,14 @@ int main(int argc, char** argv) {
 
     try {
         std::filesystem::path inpath(argv[1]);
-        std::ifstream istream(inpath, std::ios::in | std::ios::binary);
-        auto input = std::istreambuf_iterator(istream);
+        io::memory_mapped_file infile(inpath);
 
         std::filesystem::path outpath(inpath);
         outpath.replace_extension(outpath.extension().string() + ".dec");
         std::ofstream ostream(outpath, std::ios::out | std::ios::binary);
-        while (input != std::istreambuf_iterator<char>()) {
-            uint64_t decoded = vbyte::decode(&input);
+        auto it = infile.begin();
+        while (it != infile.end()) {
+            uint64_t decoded = vbyte::decode(&it);
             ostream.write((const char*) &decoded, sizeof(decoded));
         }
         ostream.flush();
