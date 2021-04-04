756bf8bdde0d350d3159d3ecfdb69e74a8820387
Sami Kukkonen <sami@samikukkonen.fi>
2021-04-04 14:58:55 +0300

    Fix getprogname() not being defined in Linux

--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,14 @@
 #include <exception>
 #include <cstdio>
 
+#if defined(__APPLE__) || defined(__FreeBSD__)
+extern "C" const char* getprogname();
+#elif defined(_GNU_SOURCE)
+static inline const char* getprogname() noexcept {
+    return program_invocation_short_name;
+}
+#endif
+
 void error(const char* message) {
     fprintf(stderr, "%s: %s\n", getprogname(), message);
 }
