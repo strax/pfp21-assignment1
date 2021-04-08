22b38838907b7d373af708570279d0863ba4002e
Sami Kukkonen <sami@samikukkonen.fi>
2021-04-08 18:40:21 +0300

    Fix vbyteencsorted

--- src/vbyteencsorted.cpp
+++ src/vbyteencsorted.cpp
@@ -2,6 +2,7 @@
 #include <iterator>
 #include <filesystem>
 #include <vector>
+#include <numeric>
 
 #include "io/memory_mapped_file.h"
 #include "io/file_writer.h"
@@ -17,10 +18,16 @@ int main(int argc, char** argv) {
     try {
         std::filesystem::path inpath(argv[1]);
         io::memory_mapped_file<uint64_t> infile(inpath);
+        // Not sure if std::copy is sequential or not
+        infile.advise(io::advise::willneed);
         std::filesystem::path outpath(inpath);
 
-        std::vector<uint64_t> inputs;
-        std::copy(infile.begin(), infile.end(), std::back_inserter(inputs));
+        // Create a separate vector with the same size as the mapped region - the constructor variant actually creates the uints
+        // instead of just reserving size (so we can use `.begin()`)
+        std::vector<uint64_t> inputs(infile.size());
+        std::copy(infile.begin(), infile.end(), inputs.begin());
+        // This could be potentially accelerated with execution policies, but GCC requires linking with Intel TBB
+        // and external libraries were forbidden in the assignment brief
         std::sort(inputs.begin(), inputs.end());
 
         outpath.replace_extension(outpath.extension().string() + ".sorted.vb");
@@ -28,7 +35,7 @@ int main(int argc, char** argv) {
 
         uint64_t previous = 0;
         auto output = outfile.begin();
-        for (const auto &n : infile) {
+        for (const auto &n : inputs) {
             uint64_t delta = n - previous;
             previous = n;
             vbyte::encode(&output, delta);
